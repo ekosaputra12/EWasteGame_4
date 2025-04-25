ea4a4e795c1e90a0012e8ce86706bd07c52dd5f5
ekosaputra12 <ekosaputra@students.amikom.ac.id>
2025-04-25 21:54:03 +0700

    update
    update

--- Assets/AssetVisualNovel/Script/GameController.cs
+++ Assets/AssetVisualNovel/Script/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
 {
     public StoryScene currentScene;
     public BottomBarController bottomBar;
+
     
     void Start()
     {
@@ -15,18 +16,25 @@ public class GameController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Tombol ditekan: Space atau Mouse Click");
+
             if (bottomBar.IsCompleted())
             {
+                Debug.Log("Scene sudah selesai.");
+
                 if (bottomBar.IsLastSentence())
                 {
+                    Debug.Log("Ini adalah kalimat terakhir.");
                     currentScene = currentScene.nextScene;
                     bottomBar.PlayScene(currentScene);
                 }
+
                 bottomBar.PlayNextSentence();
             }
         }
     }
+
 }
